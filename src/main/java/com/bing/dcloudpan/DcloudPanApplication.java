cd4ef581f5f01000a1f839a6ae5ad85db81dcc26
libing <15040855446@163.com>
2025-08-27 21:19:43 +0800

    init: 初始AI云盘工程

---
+++ src/main/java/com/bing/dcloudpan/DcloudPanApplication.java
@@ -0,0 +1,13 @@
+package com.bing.dcloudpan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcloudPanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcloudPanApplication.class, args);
+    }
+
+}
