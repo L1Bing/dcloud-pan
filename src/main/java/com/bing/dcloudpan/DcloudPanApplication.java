d9c9b25d07efd4edb04186caf1f058c9b4060a22
libing <15040855446@163.com>
2025-09-10 22:57:54 +0800

    feat: 增加接口文档Knife4j

--- src/main/java/com/bing/dcloudpan/DcloudPanApplication.java
+++ src/main/java/com/bing/dcloudpan/DcloudPanApplication.java
@@ -1,13 +1,33 @@
 package com.bing.dcloudpan;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
+@Slf4j
 public class DcloudPanApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DcloudPanApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(DcloudPanApplication.class, args);
+        Environment env = application.getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t"+
+                        "API文档: \thttp://{}:{}/doc.html\n"+
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
     }
 
 }
